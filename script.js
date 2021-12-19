6b8955b2ac8d050fd816993e012d34c4a73e83da
Pavel Trushnikov <oracal@mail.ru>
2021-12-19 21:19:27 +0300

    Добавил форму, таблицу в Index.html, Добавил классы в Script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert('Начало приложения');
+
+const dataWorker = [];
+
+class Worker {
+    constructor(firstName, lastName, age, children, dateWorkStart) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.children = children;
+        this.dateWorkStart = dateWorkStart;
+    }
+}
+
+//Оклад
+//Должность
+
+class Driver extends Worker{
+    constructor(firstName, lastName, age, children, dateWorkStart) {
+        super();
+    }
+}
+
+class Locksmith extends Worker{
+    constructor(firstName, lastName, age, children, dateWorkStart) {
+        super();
+    }
+
+}
+
+const driver = new Driver('Иван', 'Иванов', 38, true, '21.01.2021');
+
+console.log(driver);
